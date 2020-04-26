8af2d1a4d13fb1a47aa83d1c470126c0b6b1c52b
swayli94 <swayli94@gmail.com>
2020-04-26 17:24:39 +0800

    setup

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 from setuptools import setup, find_packages
 from pyTSFoil import __version__, __name__
 
-with open('Readme.md') as f:
+with open('README.md') as f:
       long_description = f.read()
 
 setup(name=__name__,
       version=__version__,
-      description='This is a python interface of TSFOIL2 (transonic small disturbance theory for foils)',
-      long_description=long_description,
+      description='This is a python interface of TSFOIL2',
+#     long_description=long_description,        # somehow pypi does not recognize this long_description
       keywords=['CFD', 'TSFOIL2', 'transonic small disturbance theory'],
-      download_url='https://github.com/swayli94/cfdpost/',
+      download_url='https://github.com/swayli94/pyTSFoil',
       license='MIT',
       author='Runze LI',
       author_email='swayli94@gmail.com',
+      packages=find_packages(),
+      package_data={"":['*.f','*.exe']},
       install_requires=['numpy', 'cst-modeling3d'],
       classifiers=[
             'Programming Language :: Python :: 3'
